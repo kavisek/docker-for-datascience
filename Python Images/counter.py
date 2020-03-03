0bb2eee7644cc65031441f3afe8c94d75cda0919
Sekhon Kavi    VANM <Kavi.Sekhon@teck.com>
2020-03-03 15:02:18 -0800

    making a fancy python script

--- Python Images/counter.py
+++ Python Images/counter.py
@@ -3,6 +3,8 @@ import time
 print("\n"*2)
 print("Starting Python Script")
 
-for i range(10):
-    print('Running step 1:')
+for i in range(10):
+    print(f'Running step {i}:')
     time.sleep(3)
+
+print('Script Done!!')
